a3037ed18f7b6f27609394034ab153bbad58a9f6
Michael <yanghao13111@gmail.com>
2023-11-24 16:54:34 +0800

    final rectify

--- backTesting_logic.py
+++ backTesting_logic.py
@@ -2,18 +2,12 @@
 import pandas as pd
 
 def calculate_indicators(df):
+    df = pd.read_csv('market_data.csv')
     # 在這裡計算技術指標
     pass
 
 def backTest_strategy(df):
+    df = pd.read_csv('market_data.csv')
     # 在這裡實現您的交易策略並進行回測
     pass
 
-# 讀取數據
-df = pd.read_csv('market_data.csv')
-
-# 計算指標
-calculate_indicators(df)
-
-# 執行回測
-backTest_strategy(df)
