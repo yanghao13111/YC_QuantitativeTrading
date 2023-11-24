7c913499744eafef1f0f53cf4063a739a5ee0252
Michael <yanghao13111@gmail.com>
2023-11-24 16:34:46 +0800

    rectify

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta
 # 假設這裡是一些參數設置
 symbol = 'BTC/USDT'
 timeframe = '1h'
-since = '2023-01-01T00:00:00Z'
 
 # 計算10天前的日期
 ten_days_ago = datetime.utcnow() - timedelta(days=10)
@@ -19,4 +18,3 @@ df = data_collection.collect_data(symbol, timeframe, since)
 df.to_csv('market_data.csv', index=False)
 
 # 調用回測邏輯
-backTesting_logic.backtest_strategy(df)
