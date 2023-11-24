1d816ca76852d76ef509c9a713185ed27c99086f
Tony Chen <tony901014@gmail.com>
2023-11-24 17:47:33 +0800

    create plot

--- backTesting_logic.py
+++ backTesting_logic.py
@@ -2,7 +2,6 @@ import backtrader as bt
 import os
 from datetime import datetime
 
-
 class MultiMATradingStrategy(bt.Strategy):
     params = (
         ("ma5_period", 5),
@@ -15,24 +14,40 @@ class MultiMATradingStrategy(bt.Strategy):
 
     def __init__(self):
         # 初始化移动平均线指标
-        self.ma5 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma5_period)
-        self.ma10 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma10_period)
-        self.ma20 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma20_period)
-        self.ma50 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma50_period)
-        self.ma100 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma100_period)
-        self.ma200 = bt.indicators.MovingAverageSimple(self.data.close, period=self.params.ma200_period)
+        self.ma5 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma5_period)
+        self.ma10 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma10_period)
+        self.ma20 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma20_period)
+        self.ma50 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma50_period)
+        self.ma100 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma100_period)
+        self.ma200 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.ma200_period)
+
+        self.order = None
 
     def next(self):
+        # 取消未完成的订单
+        if self.order:
+            self.cancel(self.order)
+
         # 入场条件
-        if (self.ma5[-1] <= self.ma10[-1] and self.ma5[0] > self.ma10[0]):
-            self.buy()
+        if not self.position and self.ma5[-1] <= self.ma10[-1] and self.ma5[0] > self.ma10[0]:
+            self.order = self.buy()
 
         # 出场条件
-        elif (self.ma5[0] < self.ma10[0] or 
-              self.ma10[0] < self.ma20[0] or
-              self.data.close[0] < self.ma50[0]):
-            self.close()
+        elif self.position and (self.ma5[0] < self.ma10[0] or self.ma10[0] < self.ma20[0] or self.data.close[0] < self.ma50[0]):
+            self.order = self.close()
 
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm {order.executed.comm}')
+            elif order.issell():
+                self.log(f'SELL EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm {order.executed.comm}')
+            self.bar_executed = len(self)
+
+    def log(self, txt, dt=None):
+        ''' Logging function '''
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f'{dt.isoformat()} {txt}')
 
 # 创建回测环境
 cerebro = bt.Cerebro()
@@ -47,15 +62,15 @@ data = bt.feeds.GenericCSVData(
     nullvalue=0.0,
     dtformat=('%Y-%m-%d %H:%M:%S'),
     datetime=0,
-    time=-1,  # 没有单独的时间列
+    time=-1,
     open=1,
     high=2,
     low=3,
     close=4,
     volume=5,
     openinterest=-1,
-    timeframe=bt.TimeFrame.Minutes,  # 由于数据是每小时的，我们将时间框架设置为分钟
-    compression=60  # 将数据压缩设置为每 60 分钟
+    timeframe=bt.TimeFrame.Minutes,
+    compression=60
 )
 
 cerebro.adddata(data)
@@ -75,3 +90,5 @@ cerebro.run()
 # 打印最终结果
 print(f'final: {cerebro.broker.getvalue()}')
 
+# 绘制结果
+cerebro.plot(style='candlestick')
